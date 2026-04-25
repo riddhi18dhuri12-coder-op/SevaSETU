ab701dc737b39156e4c8a76a7f5cbc2cd5e786e4
riddhi18dhuri12-coder-op <riddhi18dhuri12@gmail.com>
2026-04-25 21:07:45 +0530

    Create Dashboard.jsx

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,10 @@
+import MapView from "../components/MapView";
+
+export default function Dashboard() {
+  return (
+    <div>
+      <h1>ReliefSync Dashboard</h1>
+      <MapView />
+    </div>
+  );
+}
