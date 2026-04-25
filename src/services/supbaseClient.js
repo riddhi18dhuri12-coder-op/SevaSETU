2a34f4463e190c0ebd4c64c9290ef016f71ffb51
riddhi18dhuri12-coder-op <riddhi18dhuri12@gmail.com>
2026-04-25 22:42:02 +0530

    Delete src directory

--- src/services/supbaseClient.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-//  These values come from your .env file
-// Replace them in .env, NOT here
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
